527e7b451200d8b44cb97ac4d5354b414f6fa2fa
NT - Ernesto Ramirez <NearTox@outlook.com>
2016-11-10 19:28:28 -0600

    upload

--- proyecto/weed/views.py
+++ proyecto/weed/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
-form 
+from .models import Weed
 # Create your views here.
 
 class GetWeed(View):
   def get(sel, resquet,*args,**kargs):
-
+    return render()
 
 class NewWeed(View):
   def post(sel, resquet,*args,**kargs):
